48bbadf058d1c9c9d1d3c1c0a8af202d2d5c1f03
Reagan Barrington <reagan@barringtonfamily.net>
2020-04-06 16:14:01 -0400

    Returns the user object with the specified id.

--- index.js
+++ index.js
@@ -14,23 +14,42 @@ let users = [
 server.use(express.json()); // teaches the server to parse JSON from the body
 
 // endpoints
-
+// check for API running:
 server.get("/", (req, res) => {
     res.json({ api: "running....." });
   });
 
-// Returns an array users.
+// Returns an array users:
 server.get("/api/users", (req, res) => {
-    res.json(users);
+    if (users) {
+        res.json(users);
+    } else {
+        // if any errors retrieving users, 500 (Internal Server Error)
+        res.status(500).json({ errorMessage: "The users information could not be retrieved."})
+    }
 })
-// Creates a user using the information sent inside the request body.
+// Creates a user using the information sent inside the request body:
 server.post("/api/users", (req, res) => {
     const userInfo = req.body;
     if (userInfo.name == null || userInfo.bio == null){
+        // if missing name or bio, 400 (Bad Request)
         res.status(400).json({ errorMessage: "Please provide name and bio for the user."})
     } else {
+        // save new user to database, 201 (Created)
         users.push(userInfo);
         res.status(201).json(users);
+    }
+    // How do you check for errors saving the user?
+})
+// Returns the user object with the specified id:
+server.get("/api/users/:id", (req, res) => {
+    const id = req.params.id;
+    // find user with id that matches id of param:
+    const user = users.find((user) => user.id == id);
+    if (user) {
+        res.status(200).json(user);
+    } else {
+        res.status(404).json({ message: "The user with the specified ID does not exist."})
     }
 })
 
