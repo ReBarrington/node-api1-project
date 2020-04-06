aa9808ad06a285e8577cca4e72c6afbafbbfe824
Reagan Barrington <reagan@barringtonfamily.net>
2020-04-06 17:08:59 -0400

    Comments for adding HTTP status code  500 errors

--- index.js
+++ index.js
@@ -36,11 +36,11 @@ server.post("/api/users", (req, res) => {
         // if missing name or bio, 400 (Bad Request)
         res.status(400).json({ errorMessage: "Please provide name and bio for the user."})
     } else {
-        // save new user to database, 201 (Created)
+        // if valid: save new user to database, 201 (Created)
         users.push(userInfo);
         res.status(201).json(users);
     }
-    // How do you check for errors saving the user?
+    // How do you check for errors saving the user (500)?
 })
 
 // Returns the user object with the specified id:
@@ -53,6 +53,7 @@ server.get("/api/users/:id", (req, res) => {
     } else {
         res.status(404).json({ message: "The user with the specified ID does not exist."})
     }
+    // How do you check if there's an error in retrieving the user from the database (500)?
 })
 
 // Removes the user with the specified id and returns the deleted user:
@@ -66,6 +67,7 @@ server.delete("/api/users/:id", (req, res) => {
     } else {
         res.status(404).json({ message: "The user with the specified ID does not exist."})
     }
+    // How do you check if there's an error in removing the user from the database (500)?
 })
 
 
